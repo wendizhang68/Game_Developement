ff79648a6724570a8be1a49c67564cd504d7ea52
Wendi Zhang <wendizhang@berkeley.edu>
2018-09-18 20:00:30 -0700

    updated

---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_controller : MonoBehaviour
+{
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+    public Text countText;
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public Text winText;
+    private int count;
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        setcounttext();
+        winText.text = " ";
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setcounttext();
+        }
+    }
+
+    void setcounttext(){
+        countText.text = "Dotdotdot: " + count.ToString();
+        if (count >= 8){
+            winText.text = "Clear!";
+        }
+    }
+}
