b6244d671a0ae3e7b64f94c548c084806f656b17
Wendi Zhang <wendizhang@berkeley.edu>
2018-09-18 22:11:38 -0700

    finish part2

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New UFO", menuName = "UFO")]
+public class Player : ScriptableObject {
+    public string playername;
+    public string description;
+    public Sprite artwork;
+
+    public int eat;
+    public int collection;
+
+    public void Print(){
+        Debug.Log(name + ":" + description + "the player" + playername);
+    }
+		
+	
+}
---
+++ Assets/Scripts/PlayerDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDisplay : MonoBehaviour {
+    public Player ufo;
+    // Use this for initialization
+
+    public Text nameText;
+    public Text descriptionText;
+    public Image artworkImage;
+
+	void Start () {
+        //ufo.Print();
+        nameText.text = ufo.name;
+        descriptionText.text = ufo.description;
+
+	}
+}
